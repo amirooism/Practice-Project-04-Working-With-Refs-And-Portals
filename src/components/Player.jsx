103abd71ddf811ae55061bc260fc7ab70d43dc2c
amiro hatami <amirohatami@amiros-Laptop.local>
2024-06-26 08:55:40 +0330

    Using Ref instead of States

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,10 +1,19 @@
+import { useRef, useState } from "react";
+
 export default function Player() {
+  const [enteredPlayerName, setEnteredPlayerName] = useState(null);
+  const AroosakZade = useRef();
+
+  function handleButton() {
+    setEnteredPlayerName(AroosakZade.current.value)
+    AroosakZade.current.value = ' '
+  }
   return (
     <section id="player">
-      <h2>Welcome unknown entity</h2>
+      <h2>Welcome {enteredPlayerName ?? "unknown"}</h2>
       <p>
-        <input type="text" />
-        <button>Set Name</button>
+        <input type="text" ref={AroosakZade} />
+        <button onClick={handleButton}>Set Name</button>
       </p>
     </section>
   );
